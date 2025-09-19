f58e53267a9854a893891f62923ae0074260f46e
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-09-19 09:08:25 +0530

    Initial commit

--- LAB_WORK/EXP2/Diamond_pattern.java
+++ LAB_WORK/EXP2/Diamond_pattern.java
@@ -1,33 +1,50 @@
-package LAB_WORK.EXP2;
+
+/* Write a Java program to display the character rhombus structure. You can assume a character to create rhombus/diamond, left and right triangle, pyramid.  
+ Definition of Done: 
+DoD1: The program asks the user for the number of rows for the rhombus structure from the user 
+DoD 2: After a successful run, the program should ask the user if he wants to try the program for some other value, and should exit only when the user wants to exit (by pressing the character ‘N’).
+ */
+import java.util.*;
 public class Diamond_pattern {
-    public void printDiamond() {
-        int n = 5; // Number of rows for the upper half of the diamond
-
-        // Upper half of the diamond
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        // Lower half of the diamond
-        for (int i = n - 1; i >= 1; i--) {
-            for (int j = n; j > i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
-        Diamond_pattern diamond = new Diamond_pattern();
-        diamond.printDiamond();
+        Scanner scan = new Scanner(System.in);
+        char ch = '*'; // Character to use for the pattern
+        char choice;
+
+        do {
+            System.out.print("Enter the number of rows for the rhombus structure: ");
+            int rows = scan.nextInt();
+
+            // Upper half of the diamond
+            for (int i = 1; i <= rows; i++) {
+                // Print leading spaces
+                for (int j = rows; j > i; j--) {
+                    System.out.print(" ");
+                }
+                // Print characters
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print(ch);
+                }
+                System.out.println();
+            }
+
+            // Lower half of the diamond
+            for (int i = rows - 1; i >= 1; i--) {
+                // Print leading spaces
+                for (int j = rows; j > i; j--) {
+                    System.out.print(" ");
+                }
+                // Print characters
+                for (int k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print(ch);
+                }
+                System.out.println();
+            }
+
+            System.out.print("Do you want to try for another value? (Press 'N' to exit): ");
+            choice = scan.next().charAt(0);
+        } while (Character.toUpperCase(choice) != 'N');
+
+        System.out.println("Program exited.");
     }
 }
